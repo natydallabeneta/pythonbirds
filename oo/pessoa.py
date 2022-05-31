3f1c3cc85f73b537d0ea6aa782ab25e9b6e2492d
NataliaDallabeneta <natalia@dallabeneta.com.br>
2022-05-31 10:14:36 +0200

    Criada metodo cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,9 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
-        self.nome = nome
-        self.idade = idade
-
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
-
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
+    p = Pessoa()
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Natalia'
-    print(p.nome)
-    print(p.idade)
-
